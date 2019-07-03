e997925d7e132f81dc0476c2e1f2fd4755224fca
dev1-responsify <dev1@responsify.com>
2019-07-03 16:50:55 -0400

    testing link loop

--- blog_scraper/blog_scraper.py
+++ blog_scraper/blog_scraper.py
@@ -1,21 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
-from csv import writer
+# from csv import writer
 
 response = requests.get("https://www.responsify.com/blog/")
 
 soup = BeautifulSoup(response.text, 'html.parser')  
 
-with open('posts.csv', 'w') as csv_file:
-    csv_writer = writer(csv_file)
-    headers = ['Title', 'Link', 'Date']
-    csv_writer.writerow(headers)
-
+# with open('posts.csv', 'w') as csv_file:
+#     csv_writer = writer(csv_file)
+#     headers = ['Title', 'Link', 'Date']
+#      csv_writer.writerow(headers)
 posts = soup.find_all(class_="type-blogteaser-standard-headline text-center responsify-serif")
 
+
+links = soup.find_all('a')
+for link in links:
+    print(link.text)
+
 for post in posts:
-    link = post.find_all("a")["href"]
-    print(link)
     print(post.text)
-    csv_writer.writerow([link])
+    # csv_writer.writerow([link])
 
