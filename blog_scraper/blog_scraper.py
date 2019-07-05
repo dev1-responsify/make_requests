42f6a278d14d0f5f6d2428bbd3a7639334a97266
dev1-responsify <dev1@responsify.com>
2019-07-05 12:05:58 -0400

    blog_scraper.py added

--- blog_scraper/blog_scraper.py
+++ blog_scraper/blog_scraper.py
@@ -8,16 +8,22 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 # with open('posts.csv', 'w') as csv_file:
 #     csv_writer = writer(csv_file)
-#     headers = ['Title', 'Link', 'Date']
+#     head
+# ers = ['Title', 'Link', 'Date']
 #      csv_writer.writerow(headers)
 posts = soup.find_all(class_="type-blogteaser-standard-headline text-center responsify-serif")
 
 
-link = soup.find_all('a')
 
+#link = soup.find_all('a')
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+links = [a.get('href') for a in soup.find_all('a', href=True)]
+print(links)
+
+# linklist = []
+# for link in soup.find_all('a'):
+#     print(link.get('href'))
+    
 
 for post in posts:
     print(post.text)
