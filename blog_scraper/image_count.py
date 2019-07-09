91a30567bcf6f2757871097263ee1e0eb1c1bca8
dev1-responsify <dev1@responsify.com>
2019-07-09 09:49:22 -0400

    added image count function

--- blog_scraper/image_count.py
+++ blog_scraper/image_count.py
@@ -2,14 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 
+
 response = requests.get("http://blogs.reuters.com/")
 soup = BeautifulSoup(response.text, 'html.parser')
-links = [a.get('href') for a in soup.find_all('a', href=True)]  
 
-# posts = soup.find_all(class_="type-blogteaser-standard-headline text-center responsify-serif")
 
 def image_count(url):
+    img_tags = soup.find_all('img')
+    urls = [img['src'] for img in img_tags]
+    num_images = str(len(urls))
+    print('image count = ' +  num_images)
 
+image_count(response)
 
 
 
