24d0a33e81c0e89631e804a198379bc7f88bb6a7
Rafael Perez <dev1@responsify.com>
2019-07-16 17:53:47 -0400

    added a function that returns all the links in a page

--- blog_scraper/word_count_2.py
+++ blog_scraper/word_count_2.py
@@ -21,21 +21,39 @@ def make_req(url):
     else:
         print('An error has occurred.')
 
+
+# returns the number of words in a texts
 def count_words(text):
     lst = text.split()
     return len(lst)
 
 # returns the information from the obj in the api
+# returns error if info not there
 def get_info(res, obj):
     try: return obj['objects'][0][res]
     except: return 'object does not exist'
 
+# get all the links on that page 
+# task: determine which links are blogs 
+# challenge: some blogs don't have /blog in href ex: blogs in responsify.com/blog
+def get_links(obj):
+    raw_data = get_info('html', obj)
+    res_lst = raw_data.split('<')
+    result = set()
+    for i in res_lst:
+        if 'href' in i: # strips quotes, etc. and add link to set
+            try: result.add((i.split('=')[1]).replace('"', '').rstrip('/>'))
+            except: pass
+    return result
+
+        
 def main():
     obj = make_req('https://www.responsify.com/blog/')
     words = count_words(obj['objects'][0]['text'])
     print('title:', get_info('title', obj))
     print('author:', get_info('author', obj))
-    print('html:', get_info('html', obj))
+    links = get_links(obj)
+    for i in links: print(i)
     # print('num words:', words)
 
 main()
